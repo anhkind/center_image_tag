71b38ea1fc7622a6e6245de60aabd7146b0a2109
anhkind <anhkind@gmail.com>
2013-10-29 02:11:54 +0800

    Minor update to 'require' statement for Railtie

--- lib/center_image_tag.rb
+++ lib/center_image_tag.rb
@@ -2,7 +2,7 @@ require "action_view"
 
 require "center_image_tag/version"
 require "center_image_tag/view_helper"
-require "center_image_tag/railtie"
+require "center_image_tag/railtie" if defined?(Rails)
 
 module CenterImageTag
 
